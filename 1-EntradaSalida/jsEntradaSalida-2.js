2c45c805872500e50688ac0907dd96b7839aba29
alumno <alumno@utnfra.com.ar>
2020-02-04 12:32:06 -0300

    ejercicios programacion clase 1

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -1,7 +1,12 @@
-/*Debemos lograr tomar un nombre con 'prompt' 
+/*Debemos lograr tomar un nombre con 'prompt'
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	
+  var dato
+
+  //prompt devuelve un valor
+  dato = prompt ("Ingrese un dato");
+
+  alert (dato);
 }
 
