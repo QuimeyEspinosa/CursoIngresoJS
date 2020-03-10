2fd80b5f7550fc756f413dfd4cdc503ac23ccb4e
QuimeyEspinosa <quimey.espinosa@hotmail.com>
2020-03-10 06:11:24 -0300

    practica parcial 2020

--- 2020IngresoFuentes(febrero) SIN RESOLVER/tres.js
+++ 2020IngresoFuentes(febrero) SIN RESOLVER/tres.js
@@ -13,5 +13,110 @@ e) El promedio de edad entre los hombres solteros.^*/
 
 function mostrar()
 {
-	
-}
+    var nombrePasajero;
+    var edadPasajero;
+    var sexoPasajero;
+    var estadoCivilPasajero;
+    
+    var respuesta;
+
+    var banderaHombreCasadoMasJoven = 0;
+    var edadPasajeroHombreCasadoMasJovenMin;
+    var nombrePasajeroHombreCasadoMasJovenMin;
+
+    var banderaPasajeroMasViejo = 0;
+
+    var edadPasajeroMasViejoMax;
+    var sexoPasajeroMasViejoMax;
+    var nombrePasajeroMasViejoMax;
+
+    var contadorMujeresCasadasOViudas = 0;
+    var contadorMujeres = 0;
+    var contadorHombreSoltero = 0;
+
+    var acumuladorEdadMujeres = 0;
+    var acumuladorEdadHombreSoltero = 0;
+
+    var promedioEdadMujeres;
+    var promedioEdadHombreSoltero;
+
+
+    do {
+        do {
+            nombrePasajero = prompt("Ingrese nombre del pasajero");
+        }while(!isNaN(nombrePasajero));
+
+        do {
+            edadPasajero = prompt("Ingrese edad del pasajero");
+            edadPasajero = parseInt(edadPasajero);
+        }while(isNaN(edadPasajero) || edadPasajero < 18);
+
+        do {
+            sexoPasajero = prompt("Ingrese sexo del pasajero");
+        }while(!isNaN(sexoPasajero) || sexoPasajero != "f" && sexoPasajero != "m");
+
+        do {
+            estadoCivilPasajero = prompt("Ingrese estado civil del pasajero");
+        }while(!isNaN(estadoCivilPasajero) || estadoCivilPasajero != "casado" && estadoCivilPasajero != "soltero" && estadoCivilPasajero != "viudo");
+
+
+        //punto a)
+        if(sexoPasajero == "m" && estadoCivilPasajero == "casado" && banderaHombreCasadoMasJoven == 0 || edadPasajeroHombreCasadoMasJovenMin > edadPasajero) {
+            edadPasajeroHombreCasadoMasJovenMin = edadPasajero;
+            nombrePasajeroHombreCasadoMasJovenMin = nombrePasajero;
+
+            banderaHombreCasadoMasJoven++;
+        }
+
+        //punto b)
+        if(edadPasajero && banderaPasajeroMasViejo == 0 || edadPasajeroMasViejoMax < edadPasajero) {
+            edadPasajeroMasViejoMax = edadPasajero;
+            sexoPasajeroMasViejoMax = sexoPasajero;
+            nombrePasajeroMasViejoMax = nombrePasajero;
+
+            banderaPasajeroMasViejo++;
+        }
+
+        //punto c)
+        if(sexoPasajero == "f" && (estadoCivilPasajero == "casado" || estadoCivilPasajero == "viudo")) {
+            contadorMujeresCasadasOViudas++;
+        }
+
+        //punto d)
+        if(sexoPasajero == "f"){
+            acumuladorEdadMujeres += edadPasajero;
+            contadorMujeres++;
+        }
+        
+        //punto e)
+        if(sexoPasajero == "m" && estadoCivilPasajero == "soltero"){
+            acumuladorEdadHombreSoltero += edadPasajero;
+            contadorHombreSoltero++;
+        }
+
+        respuesta = confirm("desea continuar?");
+        
+    }while(respuesta);
+
+    //a)
+    document.write("Nombre hombre casado mas joven: " + nombrePasajeroHombreCasadoMasJovenMin + "<br>");
+
+    //b)
+    document.write("Nombre pasajero mas viejo: " + nombrePasajeroMasViejoMax + "<br>");
+    document.write("Sexo pasajero mas viejo: " + sexoPasajeroMasViejoMax + "<br>");
+
+    //c)
+    document.write("Cantidad de mujeres casadas o viudas: " + contadorMujeresCasadasOViudas + "<br>");
+
+    //d)
+    promedioEdadMujeres = acumuladorEdadMujeres / contadorMujeres;
+    document.write("Promedio edad mujeres: " + promedioEdadMujeres + "<br>");
+
+    //e)
+    promedioEdadHombreSoltero = acumuladorEdadHombreSoltero / contadorHombreSoltero;
+    document.write("Promedio edad hombres solteros: " + promedioEdadHombreSoltero + "<br>");
+
+
+
+
+}   
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
@@ -1,3 +1,16 @@
+/*Enunciado:
+Bienvenidos.
+Realizar el algoritmo que permita iterar el ingreso de dos datos:
+-una letra 
+-y un número entre -100 y 100 (validar) hasta que el usuario quiera 
+e informar al terminar el ingreso por document.write:
+a) La cantidad de números pares.
+b) La cantidad de números impares.
+c) La cantidad de ceros.
+d) El promedio de todos los números positivos ingresados.
+e) La suma de todos los números negativos.
+f) El número y la letra del máximo y el mínimo.*/
+
 function mostrar()
 {
     var respuesta;
