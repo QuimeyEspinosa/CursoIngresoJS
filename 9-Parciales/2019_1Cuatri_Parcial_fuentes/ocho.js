ffc603cb636379cf45e2efb0fe3bdac997308890
QuimeyEspinosa <quimey.espinosa@hotmail.com>
2020-03-10 07:51:47 -0300

    practica parcial 2020

--- 9-Parciales/2019_1Cuatri_Parcial_fuentes/ocho.js
+++ 9-Parciales/2019_1Cuatri_Parcial_fuentes/ocho.js
@@ -1,4 +1,110 @@
+/*Bienvenidos.
+Realizar el algoritmo que permita iterar el ingreso de dos datos, 
+-una letra 
+-y un número entre -100 y 100 (validar) hasta que el usuario quiera 
+informar al terminar el ingreso por document.write:
+a) La cantidad de números pares.
+b) La cantidad de números impares.
+c) La cantidad de ceros.
+d) El promedio de todos los números positivos ingresados.
+e) La suma de todos los números negativos.
+f) El número y la letra del máximo y el mínimo.*/
+
 function mostrar()
 {
+    var letra;
+    var numero;
+    var respuesta;
+
+    var contadorNumerosPares = 0;
+    var contadorNumerosImpares = 0;
+    var contadorCeros = 0;
+
+    var acumuladorNumerosPositivos = 0;
+    var contadorNumerosPositivos = 0;
+
+    var acumuladorNumerosNegativos = 0;
+
+    var promedioNumerosPositivos;
+
+    var banderaNumeroMinimo = 0;
+    var banderaNumeroMaximo = 0;
+
+    var numeroMin;
+    var numeroMax;
+    
+    var letraNumeroMin;
+    var letraNumeroMax;
+
+    do {
+        do {
+            letra = prompt("Ingrese una letra");
+        }while(!isNaN(letra));
+
+        do {
+            numero = prompt("Ingrese un numero entre -100 y 100");
+            numero = parseInt(numero);
+        }while(isNaN(numero) || numero < -100 || numero > 100);
+
+        //A)
+        if(numero % 2 == 0) {
+            contadorNumerosPares++;
+        //B)    
+        } else if(numero != 0) {
+            contadorNumerosImpares++; 
+        //C)   
+        } else {
+            contadorCeros++;
+        }
+
+        //D)
+        if(numero > 0) {
+            acumuladorNumerosPositivos += numero;
+            contadorNumerosPositivos++;
+            //E)
+        } else if (numero < 0) {
+            acumuladorNumerosNegativos += numero;
+        }
+
+        //F)
+        if(numero && banderaNumeroMinimo == 0 || numeroMin > numero ) {
+            numeroMin = numero;
+            letraNumeroMin = letra;
+
+            banderaNumeroMinimo++;
+        }
+
+        if(numero && banderaNumeroMaximo == 0 || numeroMax < numero ) {
+            numeroMax = numero;
+            letraNumeroMax = letra;
+
+            banderaNumeroMaximo++;
+        }
+
+        respuesta = confirm("desea continuar?");
+
+    }while(respuesta);
+
+    //A)
+    document.write("la cantidad de numeros pares es: " + contadorNumerosPares + "<br>");
+    //b)
+    document.write("la cantidad de numeros impares es: " + contadorNumerosImpares + "<br>");
+    //C)
+    document.write("la cantidad de ceros es: " + contadorCeros + "<br>");
+
+    //D)
+    promedioNumerosPositivos = acumuladorNumerosPositivos / contadorNumerosPositivos;
+    document.write("El promedio de numeros positivos es: " + promedioNumerosPositivos + "<br>");
+
+    //E)
+    document.write("La suma de todos los numeros negativos es: " + acumuladorNumerosNegativos + "<br>");
+
+    //F)
+    document.write("el numero maximo es: " + numeroMax + "<br>");
+    document.write("Y su letra es: " + letraNumeroMax + "<br>");
+
+    document.write("el numero minimo es: " + numeroMin + "<br>");    
+    document.write("Y su letra es: " + letraNumeroMin + "<br>");
+    
 
 }
--- 9-Parciales/2019_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2019_1Cuatri_Parcial_fuentes/siete.js
@@ -1,4 +1,70 @@
+/*Enunciado:
+Bienvenidos.
+Realizar el algoritmo que permita el ingreso por prompt de las 
+alturas en centimetros(validar entre 0 y 250) , 
+el sexo. (validar el sexo “f” o “m”) de 5 jugadores de básquet, informar por alert:
+a) El promedio de las alturas totales.
+b) La altura más baja y el sexo de esa persona.
+c) La cantidad de muheres que su altura supere los 190 centimetros.*/
+
 function mostrar()
 {
+    var alturaJugador;
+    var sexoJugador;
 
+    var contador;
+
+    var acumuladorAlturaJugadores = 0;
+    var contadorAlturaJugadores = 0;
+
+    var alturaJugadorMin;
+    var sexoJugadorMin;
+
+    var banderaAlturaJugador = 0;
+
+    var contadorCantidadMujeresMasAltasQue190 = 0;
+
+
+    for(contador = 0; contador < 2; contador++) {
+
+        do {
+            alturaJugador = prompt("Ingrese altura del jugador");
+            alturaJugador = parseInt(alturaJugador);
+        }while(isNaN(alturaJugador) || alturaJugador < 0 || alturaJugador > 250);
+
+        do {
+            sexoJugador = prompt("Ingrese sexo del jugador 'f' o 'm'");
+        }while(!isNaN(sexoJugador) || sexoJugador != "f" && sexoJugador != "m");
+
+        //punto a)
+        if(alturaJugador) {
+            acumuladorAlturaJugadores += alturaJugador;
+            contadorAlturaJugadores++;
+        }
+
+        //punto b)
+        if(alturaJugador && banderaAlturaJugador == 0 || alturaJugadorMin > alturaJugador) {
+            alturaJugadorMin = alturaJugador;
+            sexoJugadorMin = sexoJugador;
+
+            banderaAlturaJugador++;
+        }
+
+        //punto c)
+        if(sexoJugador == "f" && alturaJugador > 190) {
+            contadorCantidadMujeresMasAltasQue190++;
+        }
+        
+        
+    }
+
+    //a)
+    promedioAlturaJugadores = acumuladorAlturaJugadores / contadorAlturaJugadores;
+    alert("El promedio de altura de los jugadores es: " + promedioAlturaJugadores);
+
+    //b)
+    alert("El sexo del jugador con menor altura es: " + sexoJugadorMin);
+
+    //c)
+    alert("La cantidad de mujeres que superan los 190cm son: " + contadorCantidadMujeresMasAltasQue190);
 }
